f69a03735734d8b643a87ebb54ba5a4a715f63d0
Qasim Laghari <go.jets.laghari@gmail.com>
2016-04-27 10:48:53 -0500

    Serving JWT with API

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,4 +1,11 @@
 const User = require('../models/user');
+const jwt = require('jwt-simple');
+const config = require('../config');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp}, config.secret);
+}
 
 exports.signup = function(req,res,next) {
   //Check if a user exists
@@ -30,7 +37,7 @@ exports.signup = function(req,res,next) {
         }
 
         //Respond to request indicating user creation
-        res.json({ success: true});
+        res.json({ token: tokenForUser(user)});
 
       });
 
