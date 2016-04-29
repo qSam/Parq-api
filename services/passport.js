fb13122b40abeb445204f267e43555cbf5f89974
Qasim Laghari <go.jets.laghari@gmail.com>
2016-04-28 22:01:41 -0500

    Update passport for login authentication

--- services/passport.js
+++ services/passport.js
@@ -1,5 +1,30 @@
 const passport = require('passport');
-const User = require('../model/user');
+const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('require-jwt').ExtractJwt;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+// Setup options for JWT strategy
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+  secretOrKey: config.secret
+};
+
+//Create JWT strategy
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done){
+  // See if the user ID in the payload exists in our database
+  // If it does, call done with user obect
+  // otherwise, call done without a user object
+  User.findByID(payload.sub, function(err,user){
+    if (err) { return done(err,false); }
+
+    if (user) {
+      done(null, user);
+    } else {
+      done(null, false);
+    }
+  });
+});
+
+//Tell passport to use this strategy
+passport.use(jwtLogin);
